50957ea6fcb61107588808a16e48ba92c697a791
StormKing969 <wijesinghesaj@gmail.com>
2021-10-18 10:55:11 -0400

    update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$("#daily").click(function() {
+    console.log("work");
+});
+
+$("#weekly").click(function() {
+    console.log("work");
+});
+
+$("#monthly").click(function() {
+    console.log("work");
+});
